d883c8888de32e65950c71c2e9a37d9f7bac53ca
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-03-07 08:57:25 -0600

    Refractor from ShadowDog

---
+++ src/archtype/types/types.py
@@ -0,0 +1,52 @@
+import struct
+
+
+class types:
+
+    def __init__(self, endian):
+        self.endian = endian
+        self.typeDic = {
+            'byte': self.byte,
+            'bytes': self.byte_array,
+            'bytes16': self.bytes16,
+            'bytes32': self.bytes32,
+            'bytes64': self.bytes64,
+            'float32': self.float32,
+            'float64': self.float64,
+            'utf8': self.utf8
+        }
+
+    def convert(self, value, type):
+        return self.typeDic[type](value)
+
+    def byte_array(self, value):
+        return bytearray.fromhex(value)
+
+    def byte(self, value):
+        return value.to_bytes(1, 'little')
+
+    def bytes16(self, value):
+        return value.to_bytes(2, self.endian)
+
+    def bytes32(self, value):
+        return value.to_bytes(4, self.endian)
+
+    def bytes64(self, value):
+        return value.to_bytes(8, self.endian)
+
+    def float32(self, value):
+        float = self.__float_to_hex(value)
+        return float.to_bytes(4, self.endian)
+
+    def float64(self, value):
+        double = self.__double_to_hex(value)
+        return double.to_bytes(8, self.endian)
+
+    def utf8(self, value):
+        return value.encode('utf-8')
+
+    def __float_to_hex(self, f):  # https://stackoverflow.com/questions/23624212/how-to-convert-a-float-into-hex
+        return (struct.unpack('<I', struct.pack('<f', f))[0])
+
+    def __double_to_hex(self, f):
+        return (struct.unpack('<Q', struct.pack('<d', f))[0])
--- src/main.py
+++ src/main.py
@@ -10,10 +10,11 @@ from src.archtype.archtypes import Cell, Generic, Orbis
 # Easily load different architectures
 arch_dic = {
     "CELL": Cell,
-    "GENERIC": Generic, # for direct and array of bytes 
-                        # (AOB pattern find and replace)
+    "GENERIC": Generic,  # for direct and array of bytes
+    # (AOB pattern find and replace)
     "ORBIS": Orbis
 }
+
 
 def patchfile(offset, value, out, count):
     # Open and patch the output file
@@ -25,10 +26,10 @@ def patchfile(offset, value, out, count):
         logs.info(
             "\nApplied Patch Line {} in file: {}".format(count, out))
 
-def headercheck(archs, elf):
 
-    header = open(elf,"rb")
-    cell_valid_header   = b'\x7F\x45\x4C\x46\x02\x02\x01\x66'
+def headercheck(archs, elf):
+    header = open(elf, "rb")
+    cell_valid_header = b'\x7F\x45\x4C\x46\x02\x02\x01\x66'
     orbis_valid_header1 = b'\x7F\x45\x4C\x46\x02\x01\x01\x09'
     orbis_valid_header2 = b'\x2F\x6C\x69\x62\x65\x78\x65\x63\x2F\x6C\x64\x2D\x65\x6C\x66\x2E\x73\x6F\x2E\x31'
 
@@ -47,9 +48,9 @@ def headercheck(archs, elf):
             os.abort()
 
     if archs == 'orbis':
-        elf_header1_result = header.read(0x8) # save
+        elf_header1_result = header.read(0x8)  # save
         header.seek(0x4000, 0)
-        elf_header2_result = header.read(0x14) # save
+        elf_header2_result = header.read(0x14)  # save
         if elf_header1_result == orbis_valid_header1 and orbis_valid_header2 == elf_header2_result:
             logs.debug("\n"
                        "========================\n"
@@ -63,27 +64,39 @@ def headercheck(archs, elf):
             os.abort()
     header.close()
 
+
+def cloneFile(elf_file, outdate=False):
+    if outdate:
+        out = os.path.join(os.getcwd(), datetime.now().strftime(
+            '{0}-%Y-%m-%d-%H-%M-%S/{0}'.format(os.path.basename(elf_file))))
+    else:
+        out = os.path.join(os.getcwd(), ('{0}-patched/{0}'.format(os.path.basename(elf_file))))
+    # Clone the file
+    logs.info('\nSaving file to: {}'.format(out))
+    os.makedirs(os.path.dirname(out), exist_ok=True)
+    shutil.copyfile(elf_file, out)
+
+    return out
+
+
 def loadConfig(elf_file, conf_file, verbose, outdate, ci):
     # Checking desired verbosity level
-    if verbose == True:
+    if verbose:
         coloredlogs.set_level(logs.DEBUG)
-    if ci == True:
-        logs.debug('\nRunning in Buildbot Mode.')
-    else:
-        logs.debug('\nRunning in User Mode.')
+
     # Open the config file
     with open(conf_file) as fh:
         read_data = yaml.safe_load(fh)
-        if outdate == True:
-            out = os.path.join(os.getcwd(), datetime.now().strftime(
-                '{0}-%Y-%m-%d-%H-%M-%S/{0}'.format(os.path.basename(elf_file))))
-        else:
-            out = os.path.join(os.getcwd(), ('{0}-patched/{0}'.format(os.path.basename(elf_file))))
-        # Clone the file
-        logs.info('\nSaving file to: {}'.format(out))
-        os.makedirs(os.path.dirname(out), exist_ok=True)
-        shutil.copyfile(elf_file, out)
+
         for i in range(0, len(read_data)):
+            # Run headercheck() depending on run mode
+            if ci:
+                logs.debug('\nRunning in Buildbot Mode.')
+            else:
+                logs.debug('\nRunning in User Mode.')
+                # Verify the ELF file
+                headercheck(read_data[i]['arch'], elf_file)
+
             # Print Metadata
             logs.info("\n"
                       "=====================\n"
@@ -97,46 +110,39 @@ def loadConfig(elf_file, conf_file, verbose, outdate, ci):
                       "= Architecture  : {}\n"
                       "====================="
                 .format(
-                read_data[i]['game'],
-                read_data[i]['app_ver'],
-                read_data[i]['patch_ver'],
-                read_data[i]['name'],
-                read_data[i]['author'],
-                read_data[i]['note'],
-                read_data[i]['enabled'],
-                read_data[i]['arch']))
+                read_data[i]['game'], read_data[i]['app_ver'], read_data[i]['patch_ver'], read_data[i]['name'],
+                read_data[i]['author'], read_data[i]['note'], read_data[i]['enabled'], read_data[i]['arch']))
+
+            # Clone the ELF file
+            out = cloneFile(elf_file, outdate)
+
             count = 0
             if read_data[i]['enabled'] == False:
-                logs.warning('\nPatch: "{}" for "{}" is disabled and will be skipped.'
-                    .format(
-                    read_data[i]['name'],
-                    read_data[i]['game'], ))
+
+                logs.warning('\nPatch: "{}" for "{}" is disabled and will be skipped.'.format(read_data[i]['name'],
+                                                                                              read_data[i]['game'], ))
+
             else:
                 ## Load the architecture according to the config
                 if read_data[i]['arch'].upper() in arch_dic.keys():
                     architecture = arch_dic.get(read_data[i]['arch'].upper())()
-                    if ci == False:
-                        headercheck(read_data[i]['arch'], elf_file)
-                    else:
-                    # Reading patch list
-                        for patch_data in read_data[i]['patch_list']:
-                            count += 1
-                            logs.info("\nApplying patch: {}".format(count))
-                            logs.debug("\n"
-                                    "====================\n"
-                                    "= Patch Type    : {}\n"
-                                    "= Offset (Real) : {}\n"
-                                    "= Value         : {}\n"
-                                    "= OutFile       : {}\n"
-                                    "= Line          : {}\n"
-                                    "===================="
-                                    .format(patch_data[0],
-                                            hex(patch_data[1]),
-                                            patch_data[2],
-                                            out, count))
 
-                            # Process the patch according to it's architecture
-                            final_data = architecture.convertData(patch_data[0], patch_data[1], patch_data[2])
-                            patchfile(final_data.get('offset'), final_data.get('value'), out, count)
+                    # Reading patch list
+                    for patch_data in read_data[i]['patch_list']:
+                        count += 1
+                        logs.info("\nApplying patch: {}".format(count))
+                        logs.debug("\n"
+                                   "====================\n"
+                                   "= Patch Type    : {}\n"
+                                   "= Offset (Real) : {}\n"
+                                   "= Value         : {}\n"
+                                   "= OutFile       : {}\n"
+                                   "= Line          : {}\n"
+                                   "===================="
+                                   .format(patch_data[0], hex(patch_data[1]), patch_data[2], out, count))
+
+                        # Process the patch according to it's architecture
+                        final_data = architecture.convertData(patch_data[0], patch_data[1], patch_data[2])
+                        patchfile(final_data.get('offset'), final_data.get('value'), out, count)
                 else:
                     logs.error("\n{} is not a supported Architecture.".format(read_data[i]['arch']))
