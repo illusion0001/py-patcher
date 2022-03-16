9bb55361c6d9462ea1360aabd433b64c1214c2d4
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-03-16 10:01:58 -0500

    CSharp init

---
+++ csharp-port/console-patch/src/main.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* arg0 = input file
+             * arg1 = input data
+             * arg2 = offset
+             * arg3 = verbose // todo
+            */
+            var file = args[0];
+            byte[] data = Encoding.UTF8.GetBytes(args[1]);
+            long offset;
+            offset = Convert.ToInt64(args[2], 16);
+            Console.WriteLine("Arg1: " + args[0]);
+            Console.WriteLine("Arg2: " + args[1]);
+            Console.WriteLine("Arg3: " + args[2]);
+            using (var stream = File.Open(file, FileMode.Open))
+            {
+                stream.Position = offset;
+                stream.Write(data, 0, data.Length);
+            }
+            Console.WriteLine("File {0} {1} written.", file, args[2]);
+        }
+    }
+}
